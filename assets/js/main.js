67ad5f73b0396fd76d4d0ba7761bc929269b9d14
Vitor Lima <vitorb.lima102@gmail.com>
2022-11-29 20:11:27 -0300

    Adicionando o botao que altera o tema do portfolio e melhorando o codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,26 @@ function scrollToPosition(to){
         behavior: "smooth",
     })
 }
+
+/** Troca de Tema */
+const themeButton = document.getElementById('btn_theme')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
